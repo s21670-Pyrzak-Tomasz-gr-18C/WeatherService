f963cf650bd0e0965a1f1ca72617d2972d198b63
Tomasz <75432078+tpyrzak@users.noreply.github.com>
2021-08-24 20:44:17 +0200

    Wszystko działa, nie dotykać ;-)

--- src/main/java/com/Team1/mechanics/AverageDataCalculator.java
+++ src/main/java/com/Team1/mechanics/AverageDataCalculator.java
@@ -3,28 +3,23 @@ package com.Team1.mechanics;
 public class AverageDataCalculator {
 
     public double calculateAverageTemp(double openweathertemp, double weatherstacktemp, double accuweathertemp) {
-        double avgTemp = Math.round(openweathertemp + weatherstacktemp + accuweathertemp) / 3;
-        return avgTemp;
+        return (double) Math.round(openweathertemp + weatherstacktemp + accuweathertemp) / 3;
     }
 
     public double calculateAveragePressure(double openweatherpressure, double weatherstackpressure, double accuweatherpressure) {
-        double avgPressure = Math.round(openweatherpressure + weatherstackpressure + accuweatherpressure) / 3;
-        return avgPressure;
+        return (double) (Math.round(openweatherpressure + weatherstackpressure + accuweatherpressure) / 3);
     }
 
     public double calculateAverageHumidity(double openweatherhumidity, double weatherstackhumidity, double accuweatherhumidity) {
-        double avgHumidity = Math.round(openweatherhumidity + weatherstackhumidity + accuweatherhumidity) / 3;
-        return avgHumidity;
+        return (double) (Math.round(openweatherhumidity + weatherstackhumidity + accuweatherhumidity) / 3);
     }
 
     public double calculateAverageWindSpeed(double openweatherwindspeed, double weatherstackwindspeed, double accuweatherwindspeed) {
-        double avgWindSpeed = Math.round(openweatherwindspeed + weatherstackwindspeed + accuweatherwindspeed) / 3;
-        return avgWindSpeed;
+        return (double) (Math.round(openweatherwindspeed + weatherstackwindspeed + accuweatherwindspeed) / 3);
     }
 
     public double calculateAverageWindDirection(double openweatherwinddir, double weatherstackwinddir, double accuweatherwinddir) {
-        double avgWindDir = Math.round(openweatherwinddir + weatherstackwinddir + accuweatherwinddir);
-        return avgWindDir;
+        return (double) (Math.round(openweatherwinddir + weatherstackwinddir + accuweatherwinddir)/3);
     }
 }
 
